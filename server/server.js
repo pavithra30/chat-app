9542b1c335baeee0973a47feaf4c4912d42409a2
pavithra30 <pavithra.t@evolvussolutions.com>
2018-05-11 10:18:26 +0530

    Add connection and dis connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const path=require('path');
+const http=require('http');
 const express=require('express');
+const socketIO=require('socket.io');
 
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT || 3000;
 var app=express();
-
+var server=http.createServer(app);
+var io=socketIO(server);
 
  app.use(express.static(publicPath));
+io.on('connection',(socket)=>{
+  console.log('new user connected');
 
- app.listen(port,()=>{
+socket.on('disconnect',()=>{
+console.log('disconnected from client');
+});
+});
+
+ server.listen(port,()=>{
    console.log(`server is up on port ${port}`);
  });
