74b9cb7813fd4d3f4ea4f25afbd3813902fc4f7f
pavithra30 <pavithra.t@evolvussolutions.com>
2018-05-11 12:19:57 +0530

    great new user and allert message

--- server/server.js
+++ server/server.js
@@ -12,7 +12,17 @@ var io=socketIO(server);
  app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('new user connected');
+socket.emit('newMessage',{
+  from: 'Admin',
+  text: 'welcome to the chat app',
+createdAt:new Date().getTime()
+});
 
+socket.broadcast.emit('newMessage',{
+  from: 'Admin',
+  text: 'New user joined',
+  createdAt:new Date().getTime()
+});
 
 socket.on('createMessage',(message)=>{
   console.log('createMessage',message);
@@ -21,6 +31,12 @@ socket.on('createMessage',(message)=>{
     text: message.text,
     createdAt:new Date().getTime()
   });
+
+  /*socket.broadcast.emit('newMessage',{
+    from: message.from,
+    text: message.text,
+    createdAt:new Date().getTime()
+  });*/
 });
 socket.on('disconnect',()=>{
 console.log('disconnected from client');
