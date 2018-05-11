18f9bcf08209af6fc089320cdb0475eeb7d733d1
pavithra30 <pavithra.t@evolvussolutions.com>
2018-05-11 11:20:28 +0530

    emit message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,7 @@
 var socket=io();
 socket.on('connect',function(){
   console.log('connected to server');
-socket.emit('createMessage',{
-  from: 'pavi',
-  text: 'yup, that works for me'
-});
+
 });
 
 socket.on('disconnect',function(){
--- server/server.js
+++ server/server.js
@@ -13,14 +13,15 @@ var io=socketIO(server);
 io.on('connection',(socket)=>{
   console.log('new user connected');
 
-  socket.emit('newMessage',{
-    from: 'pavi',
-    text: 'see you then',
-    createdAt:123
-  });
+
 socket.on('createMessage',(message)=>{
   console.log('createMessage',message);
-})
+  io.emit('newMessage',{
+    from: message.from,
+    text: message.text,
+    createdAt:new Date().getTime()
+  });
+});
 socket.on('disconnect',()=>{
 console.log('disconnected from client');
 });
