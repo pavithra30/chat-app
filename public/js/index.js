fc9f6126039001f995ffadce7d4b00a12f1ecb5d
pavithra30 <pavithra.t@evolvussolutions.com>
2018-05-14 16:36:17 +0530

    scroll to bottom if user reaches to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 
 var socket=io();
+
+function scrollToBottom() {
+var messages=jQuery('#messages');
+var newMessage=messages.children('li:last-child');
+var clientHeight=messages.prop('clientHeight');
+var scrollTop=messages.prop('scrollTop');
+var scrollHeight=messages.prop('scrollHeight');
+var newMessageHeight=newMessage.innerHeight();
+var lastMessageHeight=newMessage.prev().innerHeight();
+if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+  messages.scrollTop(scrollHeight);
+}
+
+}
+
 socket.on('connect',function(){
   console.log('connected to server');
 
@@ -19,7 +34,7 @@ var html=Mustache.render(template,{
 });
 
   jQuery('#messages').append(html);
-
+scrollToBottom()
 
 /*  console.log('newMessage',message);
   var li=jQuery('<li></li>');
@@ -38,7 +53,7 @@ var html=Mustache.render(template,{
 });
 
   jQuery('#messages').append(html);
-
+scrollToBottom()
 
   /*var li=jQuery('<li></li>');
   var a=jQuery('<a target="_blank">My current location</a>');
