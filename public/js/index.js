dc041760d2b3ded1be0cf6db81a23021a94cc962
pavithra30 <pavithra.t@evolvussolutions.com>
2018-05-14 15:00:48 +0530

    add mustache.js for message template

--- public/js/index.js
+++ public/js/index.js
@@ -11,22 +11,42 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage',function(message){
   var formatedTime=moment(message.createdAt).format('h:mm a');
-  console.log('newMessage',message);
+var template=jQuery('#message-template').html();
+var html=Mustache.render(template,{
+  text: message.text,
+  from: message.from,
+  createdAt: formatedTime
+});
+
+  jQuery('#messages').append(html);
+
+
+/*  console.log('newMessage',message);
   var li=jQuery('<li></li>');
   li.text(`${message.from} ${formatedTime}: ${message.text}`);
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(li);*/
 });
 
 socket.on('newLocationMessage',function(message){
   var formatedTime=moment(message.createdAt).format('h:mm a');
-  var li=jQuery('<li></li>');
+  var template=jQuery('#location-message-template').html();
+var html=Mustache.render(template,{
+  from: message.from,
+  url: message.url,
+  createdAt: formatedTime
+});
+
+  jQuery('#messages').append(html);
+
+
+  /*var li=jQuery('<li></li>');
   var a=jQuery('<a target="_blank">My current location</a>');
 
   li.text(`${message.from} ${formatedTime}:  `);
   a.attr('href',message.url);
   li.append(a);
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(li);*/
 });
 
 jQuery('#message-form').on('submit',function(e){
