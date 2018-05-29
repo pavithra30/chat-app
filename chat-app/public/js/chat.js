f3dda05892d93f2a5caeb0ee8639a3b4f50802b3
pavithra30 <pavithra.t@evolvussolutions.com>
2018-05-29 14:18:22 +0530

    Initial commit

---
+++ chat-app/public/js/chat.js
@@ -0,0 +1,115 @@
+
+var socket=io();
+
+function scrollToBottom() {
+var messages=jQuery('#messages');
+var newMessage=messages.children('li:last-child');
+var clientHeight=messages.prop('clientHeight');
+var scrollTop=messages.prop('scrollTop');
+var scrollHeight=messages.prop('scrollHeight');
+var newMessageHeight=newMessage.innerHeight();
+var lastMessageHeight=newMessage.prev().innerHeight();
+if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+  messages.scrollTop(scrollHeight);
+}
+
+}
+
+socket.on('connect',function(){
+var params=jQuery.deparam(window.location.search);
+
+  socket.emit('join',params,function (err) {
+if(err){
+  alert(err);
+window.location.href='/';
+} else{
+  console.log('No error');
+}
+});
+});
+
+socket.on('disconnect',function(){
+  console.log('disconnected from server');
+
+});
+socket.on('updateUserList',function(users){
+  console.log('User List',users);
+  var ol=jQuery('<ol></ol>');
+
+  users.forEach(function(user){
+    ol.append(jQuery('<li></li>').text(user));
+  });
+  jQuery('#users').html(ol);
+});
+
+socket.on('newMessage',function(message){
+  var formatedTime=moment(message.createdAt).format('h:mm a');
+var template=jQuery('#message-template').html();
+var html=Mustache.render(template,{
+  text: message.text,
+  from: message.from,
+  createdAt: formatedTime
+});
+
+  jQuery('#messages').append(html);
+scrollToBottom()
+
+/*  console.log('newMessage',message);
+  var li=jQuery('<li></li>');
+  li.text(`${message.from} ${formatedTime}: ${message.text}`);
+
+  jQuery('#messages').append(li);*/
+});
+
+socket.on('newLocationMessage',function(message){
+  var formatedTime=moment(message.createdAt).format('h:mm a');
+  var template=jQuery('#location-message-template').html();
+var html=Mustache.render(template,{
+  from: message.from,
+  url: message.url,
+  createdAt: formatedTime
+});
+
+  jQuery('#messages').append(html);
+scrollToBottom()
+
+  /*var li=jQuery('<li></li>');
+  var a=jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from} ${formatedTime}:  `);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);*/
+});
+
+jQuery('#message-form').on('submit',function(e){
+  e.preventDefault();
+
+  var messageTextbox=jQuery('[name=message]');
+
+  socket.emit('createMessage',{
+  
+    text: messageTextbox.val()
+  },function () {
+messageTextbox.val('')
+  });
+});
+
+var locationButton=jQuery('#send-location');
+locationButton.on('click',function(){
+  if(!navigator.geolocation){
+    return alert('geolocation not supported by your browser');
+  }
+
+locationButton.attr('disabled','disabled').text('sending location...');
+  navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send location');
+    socket.emit('createLocationMessage',{
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  },function () {
+    locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location');
+  });
+});
